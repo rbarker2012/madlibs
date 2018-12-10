e5dd9e71161f245b55bb70cdec9fb9ded20c85d9
rbarker2012 <rbarker2012@gmail.com>
2018-12-09 21:06:10 -0700

    styled generate madlib and clear form buttons

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
-            <div className="header__skew">
-            </div>
-            <h1>Bottega MadLibs</h1>
-            <p>Fill out the fields below and click and click the Generate button to see the Mad Lib story</p>
+            <div className="header__skew"></div>
+            <div className="header__border"></div>
+            <h1>Bottega Madlibs</h1>
+            <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
         </div>
     )
 }
